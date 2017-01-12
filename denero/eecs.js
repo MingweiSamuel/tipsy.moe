fa509f44adc173d89e5494f634158a4b4e6596c9
Robert <robertquitt@gmail.com>
2017-01-11 19:28:14 -0800

    Add denero

---
+++ denero/eecs.js
@@ -0,0 +1,13 @@
+//eecs.js shameless stolen from eecseecs.com/eecs.js
+var introAudio = new Audio('intro.mp3');
+var loopAudio = new Audio('loop.mp3');
+
+introAudio.load();
+loopAudio.load();
+loopAudio.loop = true;
+
+introAudio.play();
+
+setTimeout(function () {
+	loopAudio.play()
+}, 36988.752)
