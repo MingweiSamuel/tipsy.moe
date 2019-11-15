482f5344e93987f3802f8184ff8987181a913d57
Robert Quitt <robertquitt@gmail.com>
2019-11-15 12:31:10 -0800

    Fix denero autoplay issue by prompting viewer to click first

--- denero/eecs.js
+++ denero/eecs.js
@@ -1,13 +1,21 @@
 //eecs.js shamelessly stolen from eecseecs.com/eecs.js
 var introAudio = new Audio('intro.mp3');
 var loopAudio = new Audio('loop.mp3');
+var started = false;
 
 introAudio.load();
 loopAudio.load();
 loopAudio.loop = true;
 
-introAudio.play();
+document.onclick = () => {
+	if (!started) {
+		document.getElementById("clickme").remove();
+		document.getElementById("dance").play();
+		introAudio.play();
 
-setTimeout(function () {
-	loopAudio.play()
-}, 36988.752)
+		setTimeout(function () {
+			loopAudio.play()
+		}, 36988.752);
+		started = true;
+	}
+};
